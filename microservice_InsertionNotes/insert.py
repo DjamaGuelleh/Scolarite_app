dd460b4bf22d0831a7284d7a3aa331be4dae1345
Wane Mamadou Oumar <kaouwane38@gmail.com>
2023-01-05 21:31:35 +0100

    Insertion Note Api Update

--- microservice_InsertionNotes/insert.py
+++ microservice_InsertionNotes/insert.py
@@ -1,5 +1,5 @@
 import os
-from flask import jsonify, request, Flask
+from flask import jsonify, request, Flask, render_template
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -8,30 +8,29 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
-app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
-app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
-app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
+app.config["MYSQL_DATABASE_PASSWORD"] = "oooo"
+app.config["MYSQL_DATABASE_DB"] = "projet_devops"
+app.config["MYSQL_DATABASE_HOST"] = "127.0.0.1"
 mysql.init_app(app)
 
 
 @app.route("/")
+
 def index():
-    """Function to test the functionality of the API"""
-    return "Hello, world!"
+    return render_template('InsertionForm.html')
 
 
-@app.route("/Inserstion", methods=["POST"])
+@app.route("/add_note", methods=["POST"])
 def add_user():
     """Function to create a user to the MySQL database"""
-    json = request.json
-    filier = json["id_filiere"]
-    matiere = json["id_matiere"]
-    cne = json["cne"]
-    note = json["note"]
+    filier =  int(request.form.get("id_filiere"))
+    matiere = int(request.form.get("id_matiere"))
+    cne = int(request.form["cne"])
+    note = float(request.form["note"])
+ 
     if filier and matiere and cne and note and request.method == "POST":
-        sql = "INSERT INTO note(id_filiere, id_filiere, cne,note) " \
-              "VALUES(%s, %s, %s)"
+        sql = "INSERT INTO note(id_matiere, id_filiere, cne,note) " \
+              "VALUES(%s,%s, %s, %s)"
         data = (matiere, filier, cne,note)
         try:
             conn = mysql.connect()
@@ -47,6 +46,7 @@ def add_user():
             return jsonify(str(exception))
     else:
         return jsonify("remplir les champs obligatoires !")
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
